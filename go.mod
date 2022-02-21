74fd8f5cff3277cf7431cfb0b35fb3f80d74a511
lhawi <lukmanhawi@gmail.com>
2022-02-21 15:28:39 +0700

    update say punten with parameter

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/lhawi/go-say-punten
+module github.com/lhawi/go-say-punten/v2
 
 go 1.17
--- say_punten.go
+++ say_punten.go
@@ -1,5 +1,5 @@
 package go_say_punten
 
-func SayPunten() string {
-	return "Hai punten"
+func SayPunten(name string) string {
+	return "Punten" + name
 }
